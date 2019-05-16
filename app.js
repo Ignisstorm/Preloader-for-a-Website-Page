01db6c76886f361aa4746d13915ae10a96c791a3
Erika Loots <43708340+Ignisstorm@users.noreply.github.com>
2019-05-16 09:57:06 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+//need to make a function that gets rid of the preloader
+/*
+window.addEventListener('load', () => {
+const preload = document.querySelector('.preload');
+preload.classList.add('preload-finish');
+});
+*/
